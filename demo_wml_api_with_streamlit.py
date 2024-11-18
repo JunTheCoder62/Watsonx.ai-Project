e7d49f786190558052ecd3fbc29534d599ae1fba
JunTheCoder62 <parissajuniara@gmail.com>
2024-11-18 19:08:26 +0700

    init

--- demo_wml_api_with_streamlit.py
+++ demo_wml_api_with_streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 
+from ibm_watson_machine_learning import APIClient
 from ibm_watson_machine_learning.foundation_models import Model
 from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
 from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes, DecodingMethods
